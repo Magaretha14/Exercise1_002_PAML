1e0b8abbe82f2a22e84b7b1ed483265dd4eaf279
Magaretha14 <magaretha.wahyunda@gmail.com>
2023-03-30 10:20:09 +0700

    add logo Pokemon, title, and description

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,6 +5,40 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 50),
+              child: Image.network(
+                "https://www.pngmart.com/files/22/Pokemon-Yellow-Logo-Transparent-PNG.png",
+                width: 300,
+                height: 200,
+              ),
+            ),
+            Container(
+              child: const Text(
+                "Let's get started.",
+                style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 20),
+              child: const Text(
+                "There's No Sense In Going Out Of \n Your Way To Get Somebody To Like You.",
+                textAlign: TextAlign.center,
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
