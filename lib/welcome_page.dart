fb212cf40b5855cefecf43bd4ba101b18f4998fd
Magaretha14 <magaretha.wahyunda@gmail.com>
2023-03-30 09:02:25 +0700

    make text Pokedex and the description of Pokedex in WelcomePage class

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -5,6 +5,33 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 80),
+              child: const Text(
+                "Pokedex.",
+                style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 50),
+              child: const Text(
+                "Welcome to Pokedex \n You can find your favorite pokemom here",
+                style: TextStyle(color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
