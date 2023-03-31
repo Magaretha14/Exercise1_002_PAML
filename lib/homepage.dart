a4d87511b090403fdb8ffbf27d716abe570d62c8
Magaretha14 <magaretha.wahyunda@gmail.com>
2023-04-01 00:07:28 +0700

    add profile image

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -38,6 +38,20 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                 ),
+                Container(
+                  margin: const EdgeInsets.only(top: 50, left: 200),
+                  width: 60,
+                  height: 60,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.lightBlue,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(
+                          'https://assets.pokemon.com/assets/cms2/img/pokedex/full/058.png'),
+                    ),
+                  ),
+                )
               ],
             ),
           ],
