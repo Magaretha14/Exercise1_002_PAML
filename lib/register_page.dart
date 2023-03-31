75ec7e8b083299937ad58c36d6184e3e6803361d
Magaretha14 <magaretha.wahyunda@gmail.com>
2023-03-31 10:01:15 +0700

    add TextFormField for name, email, password, adn re-password in REgisterpage

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import 'homepage.dart';
+
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
@@ -11,6 +13,13 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? name;
+  String? email;
+  String? password;
+  String? repassword;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,7 +71,176 @@ class _RegisterPageState extends State<RegisterPage> {
                     color: Colors.amber,
                     fontWeight: FontWeight.bold),
               ),
-            )
+            ),
+            Form(
+              key: _formKey,
+              child: Column(children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  padding: const EdgeInsets.only(left: 20),
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    "Name",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  margin: const EdgeInsets.only(right: 20),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: 'Enter your name',
+                      hintStyle: TextStyle(fontSize: 20),
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    validator: validateName,
+                    onSaved: (value) {
+                      name = value;
+                    },
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  padding: const EdgeInsets.only(left: 20),
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    "Email",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  margin: const EdgeInsets.only(right: 20),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: 'Enter your email',
+                      hintStyle: TextStyle(fontSize: 20),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    validator: validateName,
+                    onSaved: (value) {
+                      name = value;
+                    },
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  padding: const EdgeInsets.only(left: 20),
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    "Password",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  margin: const EdgeInsets.only(right: 20),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: 'Enter your password',
+                      hintStyle: TextStyle(fontSize: 20),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.blue,
+                      ),
+                      suffixIcon: Icon(
+                        Icons.remove_red_eye,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    validator: validatePassword,
+                    onSaved: (value) {
+                      password = value;
+                    },
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  padding: const EdgeInsets.only(left: 20),
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    "Re-Password",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  margin: const EdgeInsets.only(right: 20),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: 'Re-Enter your password',
+                      hintStyle: TextStyle(fontSize: 20),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.blue,
+                      ),
+                      suffixIcon: Icon(
+                        Icons.remove_red_eye,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    validator: validatePassword,
+                    onSaved: (value) {
+                      password = value;
+                    },
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 70),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const HomePage(),
+                          ),
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(80),
+                        ),
+                        minimumSize: const Size(350, 60)),
+                    child: const Text(
+                      "Register",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
           ],
         ),
       ),
